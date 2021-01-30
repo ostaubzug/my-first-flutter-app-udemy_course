1bd1ba02d62ca5ae859d85d65fced73a6167e5e2
Oliver Staub <ostaubzug@gmail.com>
2021-01-30 17:03:56 +0100

    my first buttons

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,29 @@ void main() {
 }
 
 class MyFirstApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
-        body: Text('This is my default Text, Hello World!'),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            //! below is a anonymous function
+            RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('Answer 2 chosen!')),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
+          ],
+        ),
         appBar: AppBar(
           title: Text('My First App'),
         ),
